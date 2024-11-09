850927fb84584aa49ff3646045fd038401337f8f
soeunnPark <carol0724@naver.com>
2024-11-09 16:57:42 +0900

    refactor(PromotionType): remove none
    NONE 타입을 제거했습니다.

--- src/main/java/store/domain/promotion/PromotionType.java
+++ src/main/java/store/domain/promotion/PromotionType.java
@@ -3,8 +3,7 @@ package store.domain.promotion;
 public enum PromotionType {
     MD_RECOMMEND("MD추천상품"),
     SPECIAL_DISCOUNT("반짝할인"),
-    BUY_N_GET_ONE_FREE("N+1증정"),
-    NONE("없음");
+    BUY_N_GET_ONE_FREE("N+1증정");
 
     private final String description;
 
