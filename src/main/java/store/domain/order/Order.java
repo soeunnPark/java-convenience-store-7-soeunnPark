dcba4aae20b963be8482601657fcf95aaf69d7bc
soeunnPark <carol0724@naver.com>
2024-11-09 22:42:40 +0900

    feat(Order): modify an order
    주문을 수정할 수 있습니다.
       - [x] 프로모션 할인이 적용되지 않는 일부 수량을 주문에서 제거합니다.
       - [x] 프로모션 적용을 위한 추가 구매 수량을 주문에 추가합니다.

---
+++ src/main/java/store/domain/order/Order.java
@@ -0,0 +1,22 @@
+package store.domain.order;
+
+import java.util.Map;
+import store.domain.product.Product;
+
+public class Order {
+    private final Map<Product, Integer> order;
+
+    public Order(Map<Product, Integer> order) {
+        this.order = order;
+    }
+
+    public Order addPurchaseCount(Product product, int recommendedAdditionalPurchaseQuantity) {
+        order.put(product, order.get(product) + recommendedAdditionalPurchaseQuantity);
+        return this;
+    }
+
+    public Order excludeNonPromotionCount(Product product, int promotionNonApplicablePurchaseQuantity) {
+        order.put(product, order.get(product) - promotionNonApplicablePurchaseQuantity);
+        return this;
+    }
+}
