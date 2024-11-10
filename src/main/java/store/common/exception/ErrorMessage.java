bccc35d332cddd8ef71deb278d087268d5732327
soeunnPark <carol0724@naver.com>
2024-11-10 21:45:51 +0900

    docs(error): add an error message
    에러 메시지를 추가했습니다.

--- src/main/java/store/common/exception/ErrorMessage.java
+++ src/main/java/store/common/exception/ErrorMessage.java
@@ -3,7 +3,16 @@ package store.common.exception;
 public enum ErrorMessage {
 
     PURCHASE_QUANTITY_MUST_BE_SMALLER_THAN_STOCK_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다."),
-    PRODUCT_NOT_FOUND("존재하지 않는 상품입니다.");
+    PRODUCT_NOT_FOUND("존재하지 않는 상품입니다."),
+    INVALID_CONFIRM_RESPONSE("잘못된 입력입니다."),
+    INVALID_FILE_NAME("존재하지 않는 파일명입니다."),
+    INVALID_PROMOTION_TYPE("잘못된 프로모션 타입입니다."),
+    INVALID_STOCK_QUANTITY("잘못된 재고 수량입니다."),
+    INVALID_PRODUCT_PRICE("잘못된 제품 가격입니다."),
+    PROMOTION_NOT_EXIST("존재하지 않는 프로모션입니다."),
+    PURCHASE_QUANTITY_MUST_BE_NUMBER("구매 수량은 숫자여야 합니다."),
+    INVALID_ORDER_FORM("올바르지 않은 형식으로 입력했습니다.")
+    ;
 
     private static final String errorPrefix = "[ERROR] ";
 
