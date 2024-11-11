058dcc18df5639b19a045fdd3f71657cdc44889e
soeunnPark <carol0724@naver.com>
2024-11-11 10:15:22 +0900

    fix(OutputHandler): fix string format
    출력 형식이 다르던 문제를 해결했습니다.

--- src/main/java/store/interfaces/OutputHandler.java
+++ src/main/java/store/interfaces/OutputHandler.java
@@ -32,29 +32,28 @@ public class OutputHandler {
 
     public void printReceipt(ReceiptResponse receiptResponse) {
         System.out.println("==============W 편의점================");
-        System.out.printf("%-10s %7s %10s%n", "상품명", "수량", "금액");
+        System.out.printf("%-19s%-9s%-7s%n", "상품명", "수량", "금액");
 
-        // 주문 항목 출력
         for (ReceiptResponse.OrderResponse order : receiptResponse.ordersResponse()) {
-            System.out.printf("%-10s %7d %,10d%n", order.productName(), order.purchaseQuantity(), order.totalPrice());
+            System.out.printf("%-19s%-9s%-7s%n", order.productName(), String.valueOf(order.purchaseQuantity()), String.format("%,d", order.totalPrice()));
         }
 
         System.out.println("=============증      정===============");
 
         // 증정품 항목 출력
         for (ReceiptResponse.PromotionGiveawayResponse giveaway : receiptResponse.promotionGiveawaysResponse()) {
-            System.out.printf("%-10s %7d%n", giveaway.productName(), giveaway.freeGiveaway());
+            System.out.printf("%-19s%-9s%n", giveaway.productName(), String.valueOf(giveaway.freeGiveaway()));
         }
 
         System.out.println("====================================");
 
         // 총 구매액, 행사 할인, 멤버십 할인, 최종 결제 금액 출력
-        System.out.printf("%-10s %7d %,10d%n", "총구매액", receiptResponse.totalPurchaseCount(), receiptResponse.totalPurchaseAmount());
+        System.out.printf("%-19s%-9s%-7s%n", "총구매액", String.format("%,d",receiptResponse.totalPurchaseCount()), String.format("%,d", receiptResponse.totalPurchaseAmount()));
 
         // 할인 항목에서 0일 경우에도 항상 - 표시
-        System.out.printf("%-10s %10s %10s%n", "행사할인", "", String.format("-%,d", Math.abs(receiptResponse.promotionDiscount())));
-        System.out.printf("%-10s %10s %10s%n", "멤버십할인", "", String.format("-%,d", Math.abs(receiptResponse.membershipDiscount())));
-        System.out.printf("%-10s %10s %,10d%n", "내실돈", "", receiptResponse.payment());
+        System.out.printf("%-28s%-7s%n", "행사할인", String.format("-%,d", Math.abs(receiptResponse.promotionDiscount())));
+        System.out.printf("%-28s%-7s%n", "멤버십할인", String.format("-%,d", Math.abs(receiptResponse.membershipDiscount())));
+        System.out.printf("%-28s%-7s%n", "내실돈", String.format("%,d", Math.abs(receiptResponse.payment())));
     }
 
 
